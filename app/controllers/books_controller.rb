8554d198532b7df2ccf67817242c9b916999efef
tajimakensuke <kenyuu25@gmail.com>
2021-03-17 14:32:12 +0000

    [add]show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,17 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
+    @book = Book.find(params[:id])
+    
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edite
   end
-
-  
 
   def create
     book = Book.new(book_params)

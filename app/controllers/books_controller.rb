4ec59cc73946e19c73704b71b0d9e507eabff1e4
tajimakensuke <kenyuu25@gmail.com>
2021-03-19 15:39:41 +0000

    [add]flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,25 +6,38 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.all
   end
 
-  def edite
+  def edit
     @book = Book.find(params[:id])
-    book.update
-    redirect_to books_path
+   
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to  book_path(book.id)
+    else
+      flash.now[:alert] = "eror"
+      render books_path
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
+
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
